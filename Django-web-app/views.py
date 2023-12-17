a175a7e742342b5bd9e76e5c050798319fea37f6
CTII <clay.thompson13@gmail.com>
2023-12-17 01:49:49 -0500

    🔧🐍 update storage to boto3 in Django-web-app in ComputerVision
    - Add boto3 (updates image and label uploads).
    - Add logger within view.
    - Add Session.

--- Django-web-app/views.py
+++ Django-web-app/views.py
@@ -1,7 +1,15 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.core.files.storage import default_storage
-from .storages import S3Boto3Storage
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.utils.logging import get_logger
+from boto3.session import Session
+from botocore.exceptions import ClientError
+from botocore.config import Config
 from boto3.dynamodb import resource
+import os
+import uuid
+
+# Use the logger within your view
+logger = get_logger(__name__)
 
 @csrf_exempt
 def upload_image(request):
@@ -39,25 +47,37 @@ def upload_image(request):
         # Access AWS credentials from environment variables
         aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
         aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+        region_name = os.environ['AWS_REGION_NAME']
 
-        storage = S3Boto3Storage(
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key,
-            # Access S3 bucket name from environment variables
-            bucket_name=os.environ['S3_BUCKET_NAME'],
-            # Access AWS region name from environment variables
-            region_name=os.environ['AWS_REGION_NAME']
-        )
+        session = Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name,
+       )
+
+       # create S3 client with signature version 's3v4'
+       s3_client = session.client('s3', config=Config(signature_version='s3v4'))
 
         # Generate unique filename
         filename = f'images/{uuid.uuid4()}{os.path.splitext(image.name)[1]}'
 
-        # Save image to storage
+        # Upload image to s3 bucket
         try:
-            storage.save(filename, image)
+            s3_client.put_object(
+                Bucket=os.environ['S3_BUCKET_NAME'],
+                Key=filename,
+                Body=image,
+            )
+        except ClientError as e:
+            # handle S3-specific error
+            logger.error(f'Error uploading image to S3: {e}')
+            return HttpResponseServerError(f'Error uploading image to S3: {e}')
         except Exception as e:
+            logger.error(f'Unexpected error uploading image:{e}')
             return HttpResponseServerError(f'Error uploading image: {e}')
 
+        
+        
         # Create a DynamoDB client
         dynamodb = resource('dynamodb')
         table = dynamodb.Table('your_table_name')
@@ -67,20 +87,27 @@ def upload_image(request):
             'filename': filename,
             'label': label,
             # Add any other relevant fields to the item
-            # ...
+            # ...Time Stamp? User upload (when we add sign in)
         }
 
         # Save the item to DynamoDB
         try:
             table.put_item(Item=item)
+        except ClientError as e
+            logger.error(f'DynamoDB client error: {e}')
+            # handle specific client errors
+        except ResourceNotFoundError as e:
+            logger.error(f'DynamoDB resource not found: {e}')
+            #handle resource not found error
         except Exception as e:
-            logger.error(f'Error saving item to DynamoDB: {e}')
+            logger.error(f'Unexpected error saving item to DynamoDB':{e})
+            # handle other unexpected errors
 
         # Prepare response data
         response_data = {
             'message': 'Image uploaded successfully!',
             'filename': filename,
-            'url': storage.url(filename),
+            'url': s3_client.generate_presigned_url('get_object', Params={'Bucket': os.environ['S3_BUCKET_NAME'], 'Key': filename})
         }
 
         # Return JSON response
